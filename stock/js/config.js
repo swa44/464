d3c22da56891efd73aa424286afcfc8eeacf5614
swa44 <4446244@gmail.com>
2026-04-10 13:55:54 +0900

    464업데이트

--- stock/js/config.js
+++ stock/js/config.js
@@ -1,7 +1,7 @@
 // Supabase 설정
-const SUPABASE_URL = "https://rgfvubjdjyuitprsagkx.supabase.co";
+const SUPABASE_URL = "https://zkogemvwkrjhttjvachs.supabase.co";
 const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJnZnZ1Ympkanl1aXRwcnNhZ2t4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg2NDEzODEsImV4cCI6MjA4NDIxNzM4MX0.yNmb9v4ncLlOaMZ52-yu9EJ3CLV0yI_AGE_m0fX3SOk";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inprb2dlbXZ3a3JqaHR0anZhY2hzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU2NjQ5MTcsImV4cCI6MjA5MTI0MDkxN30.jvhfLGDxpYyuOnXXRxQLU6rauzSOcWxcgBZdbvjdUZk";
 
 // Supabase 클라이언트 초기화
 const supabaseClient = window.supabase.createClient(
